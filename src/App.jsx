925af41256a90bc70d42aa0235ea081377656c50
mazqy <mazqyyy@gmail.com>
2025-03-03 19:24:17 +0100

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Header from './Header.jsx'
+function App() {
+  return (
+    <>
+      <Header />
+      <img src="https://occ-0-7579-360.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABaquPLH5wDUt2uJyomojYKQzkCT29kgBC0Prgh19ioltIwgRzEJsEgofCSZSHUin9_fqCLaUd1U0YHMl0q7IqRxtqgzrODtuTeeZ.webp?r=412" alt="" style={{width: '100%'}}/>
+    </>
+    
+  );  
+}
+
+export default App
---
+++ src/Header.jsx
@@ -0,0 +1,30 @@
+import netflixLogo from './assets/logos/netflix-3.svg'
+import searchIcon from './assets/icons/search.svg'
+import bellIcon from './assets/icons/bell.svg'
+import accountIcon from './assets/icons/user-circle.svg'
+
+function Header(){
+    return(
+        <header>
+            <nav className = "NavBar">
+            <img className="netflixLogo" src={netflixLogo} alt="" />
+                <ul className="NavList">
+                    <li className="NavElement"><a className="Nava" href="">Home</a></li>
+                    <li className="NavElement"><a className="Nava" href="">TV Shows</a></li>
+                    <li className="NavElement"><a className="Nava" href="">Movies</a></li>
+                    <li className="NavElement"><a className="Nava" href="">New & Popular</a></li>
+                    <li className="NavElement"><a className="Nava" href="">My List</a></li>
+                    <li className="NavElement"><a className="Nava" href="">Browse by Languages</a></li>
+                </ul>
+                <ul className="secondNavList">
+                    <li className="secondNavListElement" ><img src={searchIcon} alt="" /></li>
+                    <li className="secondNavListElement" ><img src={bellIcon} alt="" /></li>
+                    <li className="secondNavListElement" ><img src={accountIcon} alt="" /></li>
+                </ul>
+            </nav>
+        </header>
+            
+    )
+}
+
+export default Header
